cfc9f721c7a6e9613b451b9995f0851f110be31e
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-06-15 18:13:14 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client'.
+import ReactDOM from 'react-dom/client';
 import {RouterProvider} from "react-router-dom";
 
 import reportWebVitals from './reportWebVitals';
