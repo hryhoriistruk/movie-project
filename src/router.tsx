ee61373f93d03de82cfe34348ddceb044111fddf
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-06-15 18:13:57 +0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter, Navigate} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom".
 
 import {MainLayOut} from "./layouts";
 import {GenresPage, MovieInfoPage, MoviesPage, SearchPage} from "./pages";
