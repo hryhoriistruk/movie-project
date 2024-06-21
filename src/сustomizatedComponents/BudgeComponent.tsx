1a1358ce12195e6a8cad9373d25e9142cc66eea9
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-06-21 22:54:23 +0300

    Create .env

---
+++ src/сustomizatedComponents/BudgeComponent.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Badge, {BadgeProps} from '@mui/material/Badge';
+import {styled} from '@mui/material/styles';
+
+
+const StyledBadge = styled(Badge)<BadgeProps>(({theme}) => ({
+    '& .MuiBadge-badge': {
+        right: -3,
+        top: 13,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+    },
+}));
+
+
+
+export {StyledBadge}
